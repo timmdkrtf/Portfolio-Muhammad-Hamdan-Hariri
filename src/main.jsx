34f51c7ba076efca6c186e9542d41ccce4fe7e59
timmdkrtf <contact@mdkrtf.co.id>
2025-09-05 03:35:00 -0500

    add PWA, setup, testing 1

--- src/main.jsx
+++ src/main.jsx
@@ -19,3 +19,16 @@ createRoot(document.getElementById('root')).render(
     <RouterProvider router ={router} />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator){
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(
+      (registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      },
+      (error) => {
+        console.log('Service Worker registration failed:', error);
+      }
+    )
+  })
+}
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest:{
+        name: 'Portfolio Muhammad Hamdan Hariri App',
+        short_name: 'Portfolio Hamdan',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#16151d',
+        theme_color: '#16151d',
+        icons: [
+          {
+            src: '/src/assets/react.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
 
 })
